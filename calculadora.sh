a72fb20c7fd8397809d70977a0589c06e70b0158
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-05-06 19:04:26 +0200

    Create calculadora.sh

---
+++ calculadora.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+# Define la lista del menú aqui
+
+PS3="Selecciona una operación: " 
+
+select opt in suma resta multiplicar dividir salir; 
+
+do 
+        case $opt in 
+                suma) 
+                        read -p "Escribe el primer número: " n1 
+                        read -p "Escribe el segundo número: " n2 
+                        echo "$n1 + $n2 = $(($n1+$n2))" 
+                        ;; 
+                resta) 
+                        read -p "Escribe el primer número: " n1 
+                        read -p "Escribe el segundo número: " n2 
+                        echo "$n1 - $n2 = $(($n1-$n2))" 
+                        ;; 
+                multiplicar) 
+                        read -p "Escribe el primer número: " n1 
+                        read -p "Escribe el segundo númnero: " n2 
+                        echo "$n1 * $n2 = $(($n1*$n2))" 
+                        ;; 
+                dividir) 
+                        read -p "Escribe el primer número: " n1 
+                        read -p "Escrib el segundo número: " n2 
+                        echo "$n1 / $n2 = $(($n1/$n2))" 
+                        ;; 
+                salir) 
+                        break 
+                        ;; 
+                *) 
+                        echo "$REPLY no es una operación válida" 
+                        ;; 
+        esac 
+done
