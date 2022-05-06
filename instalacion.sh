1ed81fbb8fe0917f62a23065258ecfc2b9f4373d
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-05-06 19:07:34 +0200

    Create instalacion.sh

---
+++ instalacion.sh
@@ -0,0 +1,5 @@
+echo "Bienvenido al asistente de instalacion de la CalculadoraBash, en este caso no hay nada que instalar, asi que solo es darle permisos de ejecucion al script"
+chmod +x ./calculadora.sh
+echo "Ya hemos acabado, ahora solo tiene que hacer en su terminal: ./calculadora.sh"
+echo "Ahora se eliminara el asistente de instalacion de la Calculadora, Gracias por descargarlo :) ahora a disfrutar"
+rm instalacion.sh
